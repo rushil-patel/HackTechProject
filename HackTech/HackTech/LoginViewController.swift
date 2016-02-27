ac4f15818b4a7429befa580ca95c12b18bf949d5
Jonathan Molina <nemolina@calpoly.edu>
2016-02-27 03:39:55 -0800

    added basic login screen, not finished though, just for demo

---
+++ HackTech/HackTech/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  HackTech
+//
+//  Created by Jonathan Molina on 2/27/16.
+//  Copyright © 2016 Rushil Patel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var usernameTextfield: UITextField!
+    
+    @IBAction func loginOnClick(sender: AnyObject) {
+        //
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
